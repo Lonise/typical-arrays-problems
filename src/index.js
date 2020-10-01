cdeea6672c9451b3dea38148fe4a21a02a11dc8c
Lonise <loshkarev.n@inbox.ru>
2020-10-01 06:13:36 +0400

    init: start typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) return 0;
+    let min = 0;
+    for (let value of array) {
+        if ( min > value) min = value;
+    };
+    return min;
+
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) return 0;
+    let max = 0;
+    for (let value of array) {
+        if ( max < value) max = value;
+    };
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) return 0;
+    let average = 0;
+    array.forEach((element) => {
+        average = average + element;
+    });
+    average = average / array.length;
+    return average;
 }
